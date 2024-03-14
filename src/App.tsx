295e6912efa3f2da806ac46852a4c14c6100a635
nikhil <nickchogale@gmail.com>
2024-03-14 19:09:57 +0530

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
+import { fabric } from "fabric";
 
 function App() {
-  return <div className="App">hello</div>;
+  useEffect(() => {
+    const canvas = new fabric.Canvas("canvas", {
+      width: 200,
+      height: 200,
+      backgroundColor: "red",
+    });
+
+    // const rect = new fabric.Rect({
+    //   width: 200,
+    //   height: 200,
+    //   fill: "red",
+    // });
+
+    // canvas.add(rect);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 className="text-[gray]">Canvas</h1>
+      <canvas id="canvas" width="300" height="300"></canvas>
+    </div>
+  );
 }
 
 export default App;
